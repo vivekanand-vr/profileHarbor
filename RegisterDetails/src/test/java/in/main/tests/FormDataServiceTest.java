e75195ebb9542c8b1760ee98f0f9c0b3eb12959f
vivekanand-vr <bendragon21@gmail.com>
2024-05-09 13:54:32 +0530

    backend test - junit

---
+++ RegisterDetails/src/test/java/in/main/tests/FormDataServiceTest.java
@@ -0,0 +1,43 @@
+package in.main.tests;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import in.main.model.FormData;
+import in.main.service.FormDataService;
+import in.main.service.IFormDataService;
+
+@SpringBootTest
+public class FormDataServiceTest {
+	
+	@Autowired
+	private IFormDataService service;
+	
+    @Test
+    public void testRegister() {
+        // Mock FormData object
+        FormData formData = new FormData();
+        formData.setName("Test User");
+        
+        LocalDate dob = LocalDate.parse("2022-01-01", DateTimeFormatter.ISO_DATE); 
+        formData.setDob(Date.valueOf(dob)); //covert to java.sql.date object
+        formData.setEmail("test@example.com");
+        formData.setPhone("1234567890");
+        formData.setProgrammingLanguage("Java");
+        formData.setExperienceLevel("Intermediate");
+        formData.setPreferredStack("Spring Boot");
+        formData.setInterestedAreas("Backend Development");
+        formData.setResume(null);
+
+        // Call register method and verify the result
+        String resultMsg = service.register(formData);
+
+        // Assert the result
+        assert resultMsg.startsWith("Details are registered in Database with id");
+    }
+}

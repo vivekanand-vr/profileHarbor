0a21c2ee8c3b7c8a07ce77afcca0fd8d1e31f0fc
vivekanand-vr <bendragon21@gmail.com>
2024-05-23 14:29:54 +0530

    Service layer & Junit test files remove

--- RegisterDetails/src/main/java/in/main/repository/IFormRepo.java
+++ RegisterDetails/src/main/java/in/main/repository/IFormRepo.java
@@ -1,10 +1,8 @@
-package in.main.dao;
+package in.main.repository;
 
 import in.main.model.FormData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IFormRepo extends JpaRepository<FormData, Integer> {
-	/*
-	 *  All methods are given by JpaRepository --> check outline for reference
-	 */
+	
 }
--- RegisterDetails/src/main/java/in/main/service/IFormDataService.java
+++
@@ -1,7 +0,0 @@
-package in.main.service;
-
-import in.main.model.FormData;
-
-public interface IFormDataService {
-	public String register(FormData data);
-}
